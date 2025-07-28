782e36c8022c61c0d7593e12e87382e8b7dac0a3
ladskof <jeraldjamesgimang@gmail.com>
2025-07-28 01:58:54 -0700

    Paldo araw araw

---
+++ src/jerald/jerald3.java
@@ -0,0 +1,21 @@
+package jerald;
+
+import java.util.Scanner;
+
+public class jerald3 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        if (num > 0) {
+            System.out.println(num + " is a positive number");
+        } else if (num < 0) {
+            System.out.println(num + " is a negative number");
+        } else {
+            System.out.println("Inputted number Zero");
+        }
+    }
+}
